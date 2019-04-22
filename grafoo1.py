54e00b8836c45c375f37baef14244eee3d20e5d4
Ever Ospino <everospino97@gmail.com>
2019-04-22 14:28:07 -0500

    creando clase vertice

--- grafoo1.py
+++ grafoo1.py
@@ -1,10 +1,12 @@
+import vertice
+
 class Grafo:
     def __init__(self):
         self.listaVertices = {}
         self.numVertices = 0
 
     def agregarVertice(self,clave):
-        self.numVertices = self.numVertices + 1
+        self.numVertices += 1
         nuevoVertice = Vertice(clave)
         self.listaVertices[clave] = nuevoVertice
         return nuevoVertice
@@ -18,7 +20,7 @@ class Grafo:
     def __contains__(self,n):     # contiene los vertices
         return n in self.listaVertices
 
-    def agregarArista(self,de,a,costo=0):
+    def agregarArista(self,de,a,costo=''):
         if de not in self.listaVertices:
             nv = self.agregarVertice(de)
         if a not in self.listaVertices:
---
+++ vertice.py
@@ -0,0 +1,18 @@
+class Vertice:
+	"""docstring for Vertice"""
+	def __init__(self, nombre):
+		self.nombre = nombre
+		self.vecinos = {}
+
+	def getNombre(self, vecino):
+		return vecino.nombre
+
+	def getVecinos(self):
+		return vecinos
+		
+	def agregarVecino(self, vecino, costo):
+		if isVecino(vecino):
+        	self.listaVertices[getNombre].append(costo)
+
+    def isVecino(self,vecino):
+    	return vecino.nombre in vecinos.keys()
